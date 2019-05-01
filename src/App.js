bb6ff826895f97912857bf8a9b662100d7fdb119
kevodanh2698 <dinhduca7@gmail.com>
2019-05-01 10:43:17 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TodoItem from './Components/TodoItem';
+import {Button} from 'reactstrap';
+import Example from './Components/Example';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div>
+          <Example />
+        </div>
     );
-  }
+  } 
 }
 
 export default App;
